8ed32d4409db7d2be71dcfee91fc66bc10fce7fd
Will Beddow <will@willbeddow.com>
2023-03-25 19:11:00 -0400

    Switch to mantine RTE

--- frontend/src/components/organisms/Editor.tsx
+++ frontend/src/components/organisms/Editor.tsx
@@ -1,35 +1,79 @@
-import React, {useState} from 'react';
-import {EditorContent, useEditor} from "@tiptap/react";
-import {StarterKit} from "@tiptap/starter-kit";
-import MenuBar from "../molecules/MenuBar";
-import {Loader} from "@mantine/core";
+import { RichTextEditor, Link } from '@mantine/tiptap';
+import {JSONContent, useEditor} from '@tiptap/react';
+import Highlight from '@tiptap/extension-highlight';
+import StarterKit from '@tiptap/starter-kit';
+import Underline from '@tiptap/extension-underline';
+import TextAlign from '@tiptap/extension-text-align';
+import Superscript from '@tiptap/extension-superscript';
+import SubScript from '@tiptap/extension-subscript';
+import {Dispatch, SetStateAction} from "react";
 
 export interface EditorProps {
     initialContent?: string
+    setEditorContent: Dispatch<SetStateAction<JSONContent>>
 }
 
-export default function Editor({ initialContent }: EditorProps) {
-    const [content, setContent] = useState(initialContent || "");
-
+export default function Editor({ initialContent, setEditorContent }: EditorProps) {
     const editor = useEditor({
         extensions: [
-            StarterKit
+            StarterKit,
+            Underline,
+            Link,
+            Superscript,
+            SubScript,
+            Highlight,
+            TextAlign.configure({ types: ['heading', 'paragraph'] }),
         ],
-        content
-    })
+        content: initialContent,
+        onUpdate: ({editor}) => {
+            // TODO: might need to debounce this
+            setEditorContent(editor.getJSON());
+        }
+    });
 
-    if (!editor) {
-        return <Loader />
-    }
+    return (
+        <RichTextEditor editor={editor}>
+            <RichTextEditor.Toolbar sticky stickyOffset={60}>
+                <RichTextEditor.ControlsGroup>
+                    <RichTextEditor.Bold />
+                    <RichTextEditor.Italic />
+                    <RichTextEditor.Underline />
+                    <RichTextEditor.Strikethrough />
+                    <RichTextEditor.ClearFormatting />
+                    <RichTextEditor.Highlight />
+                    <RichTextEditor.Code />
+                </RichTextEditor.ControlsGroup>
 
-    return <div>
-        <button onClick={() => {
-            const editorContent = editor?.getJSON();
-            console.log("Editor content", editorContent)
-        }}>
-            Get content
-        </button>
-        <MenuBar editor={editor} />
-        <EditorContent editor={editor} />
-    </div>
+                <RichTextEditor.ControlsGroup>
+                    <RichTextEditor.H1 />
+                    <RichTextEditor.H2 />
+                    <RichTextEditor.H3 />
+                    <RichTextEditor.H4 />
+                </RichTextEditor.ControlsGroup>
+
+                <RichTextEditor.ControlsGroup>
+                    <RichTextEditor.Blockquote />
+                    <RichTextEditor.Hr />
+                    <RichTextEditor.BulletList />
+                    <RichTextEditor.OrderedList />
+                    <RichTextEditor.Subscript />
+                    <RichTextEditor.Superscript />
+                </RichTextEditor.ControlsGroup>
+
+                <RichTextEditor.ControlsGroup>
+                    <RichTextEditor.Link />
+                    <RichTextEditor.Unlink />
+                </RichTextEditor.ControlsGroup>
+
+                <RichTextEditor.ControlsGroup>
+                    <RichTextEditor.AlignLeft />
+                    <RichTextEditor.AlignCenter />
+                    <RichTextEditor.AlignJustify />
+                    <RichTextEditor.AlignRight />
+                </RichTextEditor.ControlsGroup>
+            </RichTextEditor.Toolbar>
+
+            <RichTextEditor.Content />
+        </RichTextEditor>
+    );
 }
--- frontend/src/pages/CreateDocument.tsx
+++ frontend/src/pages/CreateDocument.tsx
@@ -1,16 +1,18 @@
 import React, {useState} from 'react';
 import {Button, Divider, Stack, TextInput, Title} from "@mantine/core";
 import Editor from "../components/organisms/Editor";
+import {JSONContent} from "@tiptap/react";
 
 export default function CreateDocument() {
     const [name, setName] = useState("")
+    const [content, setContent] = useState<JSONContent>();
     return <Stack>
         <Title>
             Create Document
         </Title>
         <Divider />
         <TextInput label={"Name"} value={name} onChange={(event) => setName(event.currentTarget.value)}/>
-        <Editor />
+        <Editor setEditorContent={setContent}/>
         <Button>Create +</Button>
     </Stack>
 }
