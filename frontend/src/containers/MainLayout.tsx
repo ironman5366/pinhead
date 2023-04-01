f44184059bcf13f3b7fb5748a606de1d6d541b2c
Will Beddow <will@willbeddow.com>
2023-04-01 15:53:09 -0400

    WIP sidebar

--- frontend/src/containers/MainLayout.tsx
+++ frontend/src/containers/MainLayout.tsx
@@ -1,38 +1,35 @@
 import React, {PropsWithChildren} from 'react';
-import {Container, createStyles, Header, Image, rem, Stack, Title, useMantineTheme} from "@mantine/core";
-import Logo from "../assets/images/logo.png"
+import {Container, createStyles, Grid, Header, Image, Navbar, rem, Stack, Title, useMantineTheme} from "@mantine/core";
 import Wordmark from "../components/atoms/Wordmark";
 
-const HEADER_HEIGHT = rem(128)
-
-const useStyles = createStyles((theme) => ({
-    inner: {
-        height: HEADER_HEIGHT,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    }
-}));
+const HEADER_HEIGHT = rem(128);
+const SIDEBAR_HEIGHT = `calc(100vh - ${HEADER_HEIGHT})`
 
 export default function MainLayout({children}: PropsWithChildren<any>) {
-    const {classes} = useStyles();
     const theme = useMantineTheme();
+
     return <>
         <Header height={HEADER_HEIGHT} style={{
             borderBottom: `10px solid ${theme.colors.saffron[9]}`
         }}>
             <Wordmark />
         </Header>
-        <div>
-            <Stack>
-                <Title>
-                    Documents
-                </Title>
-            </Stack>
-            <Container>
-                {children}
-            </Container>
-        </div>
-
+        <Grid style={{
+            minHeight: SIDEBAR_HEIGHT
+        }} columns={24}>
+            <Grid.Col md={5} style={{
+                borderRight: "3px solid #F3BAC0",
+                minHeight: SIDEBAR_HEIGHT
+            }}>
+                <Navbar>
+                   Test
+                </Navbar>
+            </Grid.Col>
+            <Grid.Col md={19}>
+                <Container>
+                    {children}
+                </Container>
+            </Grid.Col>
+        </Grid>
     </>
 }
